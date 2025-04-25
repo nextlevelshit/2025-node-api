770ece648278bf50576553fbd8efdad60bfe0338
Michael Czechowski <m.czechowski@pons.de>
2025-04-25 10:42:05 +0200

    add simple post endpoint

--- index.js
+++ index.js
@@ -3,6 +3,32 @@ import {port} from "./src/config/constants.js"
 
 const app = express();
 
-app.listen(port, () => {
+app.use(express.json());
+
+app.get("/api", async (req, res) => {
+    res.json({message: "test"});
+});
+
+app.post("/api", async (req, res) => {
+    try {
+        const body = req.body;
+
+        console.log(body);
+
+        res.sendStatus(201);
+    } catch (e) {
+        res.sendStatus(500);
+    }
+});
+
+const server = app.listen(port, () => {
     console.log(`Server läuft auf ${port}`);
+});
+
+process.on("SIGINT", () => {
+    console.log(`Shutting down server running on ${port}`);
+
+    server.close(() => {
+        process.exit(0);
+    });
 });
