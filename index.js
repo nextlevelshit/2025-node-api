f3abc9cb582e4c70065a50cb75cedadd826b88c3
Michael Czechowski <m.czechowski@pons.de>
2025-04-25 10:18:00 +0200

    introduce env vars for port

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import express from "express";
+import {port} from "./src/config/constants.js"
 
 const app = express();
 
-app.listen(1234, () => {
-    console.log("dat lübt");
+app.listen(port, () => {
+    console.log(`Server läuft auf ${port}`);
 });
