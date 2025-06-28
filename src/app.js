030b69d0f9b541a8f0f8e98790e9b1f2d5c7213d
Michael Czechowski <m.czechowski@pons.de>
2025-06-29 00:07:05 +0200

    feat: enable CORS middleware in app.js

--- src/app.js
+++ src/app.js
@@ -7,7 +7,19 @@ import { createApiRoutes } from "./routes/api.js";
 export function createApp(cache, options = {}) {
   const app = express();
 
+  // Enable cors
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, OPTIONS",
+    );
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
+  });
+  // Enable JSON parsing for incoming requests
   app.use(express.json());
+  // Add routes for API endpoints
   app.use("/api", createApiRoutes(cache));
 
   // Static HTML route
